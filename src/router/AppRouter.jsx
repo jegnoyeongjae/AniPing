8675a80e70af37ca73013c92253687691824c9a0
jegnoyeongjae <jyj0298@naver.com>
2025-09-12 17:38:32 +0900

    merge comp 2.0

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,19 @@
-import { Outlet } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import Header from '../components/common/Header';
-import Footer from '../components/common/Footer';
+import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
 
 const AppRoute = () => {
   const [headerProps, setHeaderProps] = useState({});
 
-  console.log('라우터 페이지 헤더 프롭', headerProps);
-
-
+  console.log("라우터 페이지 헤더 프롭", headerProps);
 
   return (
     <div>
       <Header {...headerProps} />
-      <Outlet context={{ setHeaderProps }} />
+      <div className="main-container">
+        <Outlet context={{ setHeaderProps }} />
+      </div>
       <Footer />
     </div>
   );
